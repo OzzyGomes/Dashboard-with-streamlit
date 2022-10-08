bef4fc02811b127d0c0de2fc319802248e148e34
Ozzy Gomes <ozzygomes@outlook.com>
2022-10-08 14:54:42 -0300

    Dashboard criado

--- app.py
+++ app.py
@@ -7,17 +7,21 @@ st.set_page_config(page_title="Sales Dashboard",
                 page_icon=":bar_chart:",
                 layout="wide")
 
-df = pd.read_excel(
-    io='supermarkt_sales.xlsx',
-    engine='openpyxl',
-    sheet_name='Sales',
-    skiprows=3,
-    usecols='B:R',
-    nrows=1000,
-)
+@st.cache
+def get_data_frame_excel():
+    df = pd.read_excel(
+        io='supermarkt_sales.xlsx',
+        engine='openpyxl',
+        sheet_name='Sales',
+        skiprows=3,
+        usecols='B:R',
+        nrows=1000,
+    )
+    #adicionando coluna hora
+    df["Hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+    return df
 
-
-st.dataframe(df)
+df = get_data_frame_excel()
 
 st.sidebar.header("Os filtros estão aqui:")
 
@@ -38,3 +42,90 @@ gender = st.sidebar.multiselect(
     options=df["Gender"].unique(),
     default=df["Gender"].unique()
 )
+
+df_selection = df.query(
+    "City == @city & Customer_type == @customer_type & Gender == @gender"
+
+)
+
+#st.dataframe(df_selection)
+
+
+#mainpage
+st.title(":bar_chart: Dashboard de Vendas")
+st.markdown('##')
+
+
+# principais KPI's
+total_sales = int(df_selection['Total'].sum())
+average_rating = round(df_selection['Rating'].mean(),1)
+star_rating = ":star:" * int(round(average_rating, 0))
+average_sales_by_transaction = round(df_selection["Total"].mean(), 2)
+
+left_column, middle_column, right_column = st.columns(3)
+
+with left_column:
+    st.subheader("Total de Vendas:")
+    st.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    st.subheader("Nota Média:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Média de Vendas por Transação:")
+    st.subheader(f"US $ {average_sales_by_transaction}")
+
+st.markdown("---")
+
+# Sales by product line [bar chart]
+sales_by_product_line = (
+    df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+)
+fig_product_sales = px.bar(
+    sales_by_product_line,
+    x="Total",
+    y=sales_by_product_line.index,
+    orientation="h",
+    title="<b>Vendas por Tipo de Produto</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
+    template="plotly_white",
+)
+fig_product_sales.update_layout(
+    plot_bgcolor='rgba(0,0,0,0)',
+    xaxis=(dict(showgrid=False)),
+)
+
+
+
+#sales by hour [bar chart]
+sales_by_hour = df_selection.groupby(by=["Hour"]).sum()[["Total"]]
+fig_hourly_sales = px.bar(
+    sales_by_hour,
+    x=sales_by_hour.index,
+    y="Total",
+    title="<b>Vendas por Hora</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
+    template="plotly_white",
+)
+
+fig_hourly_sales.update_layout(
+    xaxis=(dict(tickmode="linear")),
+    plot_bgcolor='rgba(0,0,0,0)',
+    yaxis=(dict(showgrid=False)),
+)
+
+left_column, right_column = st.columns(2)
+
+left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
+
+right_column.plotly_chart(fig_product_sales, use_container_width=True)
+
+#Escondendo StreamLit style
+
+hide_st_style = '''
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        </style>
+'''
+st.markdown(hide_st_style, unsafe_allow_html=True)
